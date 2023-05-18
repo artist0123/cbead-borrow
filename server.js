3c3aa17ec64dfd852c654797c7918aebab463698
artist0123 <napatart1111@gmail.com>
2023-05-18 21:48:27 +0700

    from java

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const AWS = require("aws-sdk");
-const { processPayment, savePaymentRecord } = require("./paymentService");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -19,202 +18,66 @@ AWS.config.update({
   region: "us-east-1",
 });
 
-const dynamoDb = new AWS.DynamoDB.DocumentClient();
+const dynamodb = new AWS.DynamoDB.DocumentClient();
 const tableName = "borrow";
 
-// Define your API routes here
-// ... (previous code)
+app.post("/borrow", async (req, res) => {
+  const model = req.body;
 
-// List all borrowings
-app.get("/borrow", (req, res) => {
   const params = {
-    TableName: tableName,
-  };
-
-  dynamoDb.scan(params, (error, data) => {
-    if (error) {
-      res.status(500).json({ error: "Error fetching borrowings" + error});
-    } else {
-      res.json(data.Items);
-    }
-  });
-});
-
-// Get a single borrowing by ID
-app.get("/borrow/:id", (req, res) => {
-  const params = {
-    TableName: tableName,
-    Key: {
-      id: req.params.id,
+    TableName: tableName, // Replace with your actual DynamoDB table name
+    Item: {
+      id: uuidv4(),
+      status: model.status,
+      borrow_date: model.borrow_date,
+      due_date: model.due_date,
+      late: model.late,
+      userId: model.userId,
+      booksId: model.booksId,
     },
   };
 
-  dynamoDb.get(params, (error, data) => {
-    if (error) {
-      res.status(500).json({ error: "Error fetching borrowing" + error});
-    } else {
-      res.json(data.Item);
-    }
-  });
-});
-
-// Borrow multiple books
-app.post("/books/borrow", async (req, res) => {
-  const { userId, bookIds, borrowDate, dueDate } = req.body;
-
-  if (!Array.isArray(bookIds) || bookIds.length === 0) {
-    res.status(400).json({ error: "bookIds must be a non-empty array" });
-    return;
-  }
-
   try {
-    const borrowedBooks = [];
-    for (const bookId of bookIds) {
-      // Generate a unique id
-      const id = uuidv4();
-
-      // Add a new borrow record for the book
-      const borrowRecord = {
-        id,
-        userId,
-        bookId,
-        borrowDate,
-        dueDate,
-        returnDate: null,
-        lateFine: 0,
-      };
-
-      const borrowRecordParams = {
-        TableName: tableName,
-        Item: borrowRecord,
-      };
-
-      await dynamoDb.put(borrowRecordParams).promise();
-
-      const bookParams = {
-        TableName: "books",
-        Key: { id: bookId },
-        UpdateExpression: "SET available = :available",
-        ExpressionAttributeValues: { ":available": false },
-        ReturnValues: "ALL_NEW",
-      };
-
-      const data = await dynamoDb.update(bookParams).promise();
-      borrowedBooks.push({
-        ...data.Attributes,
-        id,
-        userId,
-        borrowDate,
-        dueDate,
-      });
-    }
-
-    res.json(borrowedBooks);
-  } catch (error) {
-    res.status(500).json({ error: "Error borrowing books" + error});
+    await dynamodb.put(params).promise();
+    res.json({ message: "Borrow created successfully", id: params.Item.id });
+  } catch (err) {
+    res.status(500).json({ error: err.toString() });
   }
 });
 
-// Return multiple borrowed books
-app.post("/books/return", async (req, res) => {
-  const { userId, bookIds, returnDate, paymentInfo } = req.body;
+app.put("/borrow", async (req, res) => {
+  const model = req.body;
 
-  if (!Array.isArray(bookIds) || bookIds.length === 0) {
-    res.status(400).json({ error: "bookIds must be a non-empty array" });
-    return;
-  }
-
-  try {
-    const returnedBooks = [];
-    let totalLateFine = 0;
-
-    for (const bookId of bookIds) {
-      // Retrieve the due date from the borrow record
-      const borrowRecordParams = {
-        TableName: tableName,
-        Key: { userId, bookId },
-      };
-
-      const borrowRecordData = await dynamoDb.get(borrowRecordParams).promise();
-      const dueDate = new Date(borrowRecordData.Item.dueDate);
-
-      const actualReturnDate = new Date(returnDate);
-      const lateDays = Math.ceil(
-        (actualReturnDate - dueDate) / (1000 * 60 * 60 * 24)
-      );
-      const lateFine = lateDays > 0 ? lateDays * /* Fine rate per day */ 20 : 0;
-      totalLateFine += lateFine;
-
-      const bookParams = {
-        TableName: "books",
-        Key: { id: bookId },
-        UpdateExpression: "SET available = :available",
-        ExpressionAttributeValues: { ":available": true },
-        ReturnValues: "ALL_NEW",
-      };
-
-      const data = await dynamoDb.update(bookParams).promise();
-      returnedBooks.push({ ...data.Attributes, userId, returnDate, lateFine });
-
-      // Update the borrow record with the return date and the late fine (if any) for each book
-      const updateBorrowRecordParams = {
-        TableName: "borrow",
-        Key: { userId, bookId },
-        UpdateExpression: "SET returnDate = :returnDate, lateFine = :lateFine",
-        ExpressionAttributeValues: {
-          ":returnDate": returnDate,
-          ":lateFine": lateFine,
-        },
-        ReturnValues: "ALL_NEW",
-      };
-
-      await dynamoDb.update(updateBorrowRecordParams).promise();
-    }
-
-    if (totalLateFine > 0) {
-      const paymentSuccess = processPayment(totalLateFine, paymentInfo);
-
-      const paymentRecord = await savePaymentRecord(
-        totalLateFine,
-        userId,
-        null,
-        null,
-        totalLateFine,
-        paymentSuccess ? "success" : "failed"
-      );
-
-      if (!paymentSuccess) {
-        res.status(400).json({ error: "Payment failed", paymentRecord });
-        return;
-      }
-    }
-
-    res.json(returnedBooks);
-  } catch (error) {
-    res.status(500).json({ error: "Error returning books" + error});
-  }
-});
-
-// Delete a borrowing
-app.delete("/borrow/:id", (req, res) => {
   const params = {
-    TableName: tableName,
+    TableName: tableName, // Replace with your actual DynamoDB table name
     Key: {
-      id: req.params.id,
+      id: model.id,
+    },
+    UpdateExpression:
+      "set #status = :s, borrow_date = :bd, due_date = :dd, late = :l, userId = :uid, booksId = :bid",
+    ExpressionAttributeNames: {
+      "#status": "status",
+    },
+    ExpressionAttributeValues: {
+      ":s": model.status,
+      ":bd": model.borrow_date,
+      ":dd": model.due_date,
+      ":l": model.late,
+      ":uid": model.userId,
+      ":bid": model.booksId,
     },
   };
 
-  dynamoDb.delete(params, (error) => {
-    if (error) {
-      res.status(500).json({ error: "Error deleting borrowing" + error});
-    } else {
-      res.json({ success: true });
-    }
-  });
+  try {
+    await dynamodb.update(params).promise();
+    res.json({ message: "Borrow updated successfully" });
+  } catch (err) {
+    res.status(500).json({ error: err.toString() });
+  }
 });
 
 // ... (previous code)
 
-app.listen(port, "0.0.0.0", () => {
+app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
